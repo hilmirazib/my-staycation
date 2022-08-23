72109e552997283fe30aca1254ef73ff30dafd05
hilmi.inolabs <hilmi.inolabs@gmail.com>
2022-08-23 23:09:40 +0700

    setup project

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import 'assets/scss/style.scss';
+
+function App() {
+  return <div className="App">hy dunia</div>;
+}
+
+export default App;
---
+++ src/parts/Header.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import Button from 'elements/Button';
+export default function Header(props) {
+  return <div></div>;
+}
